6e1f533c7dfb8dbce8fd0f69ae21fc24ff28ee6d
Priti Chauhan <priti.chauhan1518@gmail.com>
2023-07-25 20:37:55 -0700

    adding home page and routing options

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { UsersComponent } from './users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'product', component: ProductsComponent },
+
+  {path: 'user', component: UsersComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
